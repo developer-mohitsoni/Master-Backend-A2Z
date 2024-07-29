0af131591c8302cf6e0d4e12842763abdc81fb3d
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-29 20:12:12 +0530

    Import ApiRoutes in server.js

--- Master Backend A2Z/server.js
+++ Master Backend A2Z/server.js
@@ -3,14 +3,24 @@ import express from "express";
 import "dotenv/config";
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+//* Middleware
+  
+app.use(express.json());
+app.use(express.urlencoded({ extends: false }));
 
 app.get("/", (req, res) => {
   return res.json({
     message: "Hello JS",
   });
 });
+
+//* Import routes
+
+import ApiRoutes from "./routes/api.js";
+
+app.use("/api", ApiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
