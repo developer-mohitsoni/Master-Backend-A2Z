830b3d3fe9bd45e540d7320b8d0088cb08546001
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-29 19:55:13 +0530

    Add AuthController.js which includes Authentication Control Check

---
+++ Master Backend A2Z/controllers/AuthController.js
@@ -0,0 +1,21 @@
+import prisma from "../DB/db.config.js";
+import { z } from "zod";
+import { registerSchema } from "../validation/authValidation.js";
+import { ZodError } from "zod";
+
+class AuthController {
+  static async register(req, res) {
+    try {
+      const body = req.body;
+      const payload = await registerSchema.parse(body);
+      return res.json({payload})
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return res.status(500).json({
+          message: "Invalid Data",
+          errors: error.message,
+        });
+      }
+    }
+  }
+}
