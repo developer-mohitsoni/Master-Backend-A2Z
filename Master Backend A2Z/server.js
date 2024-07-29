894a9c2547559f54b38de9d3a4c31cb7559ef3b5
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-29 17:58:48 +0530

    Set up basic server with express and dotenv

---
+++ Master Backend A2Z/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hello JS",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT: ${PORT}`);
+});
