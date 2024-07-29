cb56f2672f123ff4d6e752dc329c344ffd98eaef
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-29 20:10:47 +0530

    export AuthController.js File

--- Master Backend A2Z/controllers/AuthController.js
+++ Master Backend A2Z/controllers/AuthController.js
@@ -2,6 +2,7 @@ import prisma from "../DB/db.config.js";
 import { z } from "zod";
 import { registerSchema } from "../validation/authValidation.js";
 import { ZodError } from "zod";
+import { formateError } from "../validation/CustomErrorReporter.js";
 
 class AuthController {
   static async register(req, res) {
@@ -11,11 +12,15 @@ class AuthController {
       return res.json({payload})
     } catch (error) {
       if (error instanceof ZodError) {
+        const errors = formateError(error);
+
         return res.status(500).json({
           message: "Invalid Data",
-          errors: error.message,
+          errors,
         });
       }
     }
   }
 }
+
+export default AuthController
