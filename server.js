5d4d034485582a9f5cecdc3e52e217bda788d153
Mohit Soni <developer_mohitsoni@outlook.com>
2024-08-13 15:20:14 +0530

    Basic Express Server Setup

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+import "dotenv/config";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hello It's Working...",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
