ded9e1072d8b096f2f467007b21fa896630ea709
Mohit Soni <developer_mohitsoni@outlook.com>
2024-08-13 17:39:42 +0530

    Add login endpoint with validation and error handling

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,6 @@
 import { errors } from "@vinejs/vine";
 import prisma from "../DB/db.config.js";
-import { registerSchema } from "../validations/authValidation.js";
+import { loginSchema, registerSchema } from "../validations/authValidation.js";
 
 import vine from "@vinejs/vine";
 
@@ -33,7 +33,7 @@ class AuthController {
       }
 
       //* If user email is new email then :-
-      
+
       if (!findUser) {
         //* Encrypt the password
 
@@ -52,6 +52,35 @@ class AuthController {
           user,
         });
       }
+    } catch (error) {
+      console.log("The error is: ", error);
+
+      if (error instanceof errors.E_VALIDATION_ERROR) {
+        // console.log(error.messages);
+
+        return res.status(400).json({
+          errors: error.messages,
+        });
+      } else {
+        return res.status(500).json({
+          status: 500,
+          message: "Something went wrong... Please try again",
+        });
+      }
+    }
+  }
+
+  static async login(req, res) {
+    try {
+      const body = req.body;
+
+      const validator = vine.compile(loginSchema);
+
+      const payload = await validator.validate(body);
+
+      return res.json({
+        payload,
+      });
     } catch (error) {
       console.log("The error is: ", error);
 
