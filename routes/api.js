f7a9f28282ebca0a8f7da12ce26b3d702d3526cb
Mohit Soni <developer_mohitsoni@outlook.com>
2024-08-13 19:44:32 +0530

    import authMiddleware from middleware/Authenticate.js Folder

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import AuthController from "../controllers/AuthController.js";
+import ProfileController from "../controllers/ProfileController.js";
+
+import authMiddleware from "../middleware/Authenticate.js";
 
 const router = Router();
 
 router.post("/auth/register", AuthController.register);
 router.post("/auth/login", AuthController.login)
+
+
+//*  Profile routes
+
+router.get("/profile", authMiddleware, ProfileController.index) //! Private Route
 
 export default router;
