bdf7cb6d1564820b9a3cdd6b511a281a3be36f22
Mohit Soni <developer_mohitsoni@outlook.com>
2024-08-13 17:33:18 +0530

    Complete AuthRegister Endpoint where it includes to check for uniqueEmail of user, bcrypt/hash the password of user who have unique email then save to it on our database with some Error Handling

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,6 +4,8 @@ import { registerSchema } from "../validations/authValidation.js";
 
 import vine from "@vinejs/vine";
 
+import bcrypt from "bcrypt";
+
 class AuthController {
   static async register(req, res) {
     try {
@@ -13,15 +15,56 @@ class AuthController {
 
       const payload = await validator.validate(body);
 
-      return res.json({
-        payload,
+      //* Check if user email already exist
+
+      const findUser = await prisma.users.findUnique({
+        where: {
+          email: payload.email,
+        },
       });
+
+      if (findUser) {
+        return res.status(400).json({
+          errors: {
+            email:
+              "User Email Already Exist. Please Use Another Email to Register",
+          },
+        });
+      }
+
+      //* If user email is new email then :-
+      
+      if (!findUser) {
+        //* Encrypt the password
+
+        const salt = bcrypt.genSaltSync(10);
+        payload.password = bcrypt.hashSync(payload.password, salt);
+
+        //*  Now, Store into our DB
+
+        const user = await prisma.users.create({
+          data: payload,
+        });
+
+        return res.json({
+          status: 200,
+          message: "User Created Successfully",
+          user,
+        });
+      }
     } catch (error) {
+      console.log("The error is: ", error);
+
       if (error instanceof errors.E_VALIDATION_ERROR) {
         // console.log(error.messages);
 
         return res.status(400).json({
           errors: error.messages,
+        });
+      } else {
+        return res.status(500).json({
+          status: 500,
+          message: "Something went wrong... Please try again",
         });
       }
     }
