d3cec6c6ae91eaa81671dfab208c9435af9faeb2
Mohit Soni <developer_mohitsoni@outlook.com>
2024-08-16 14:37:54 +0530

    Include reporter information in news transformation

--- transform/newsApiTransform.js
+++ transform/newsApiTransform.js
@@ -8,6 +8,14 @@ class newsApiTransform {
       news: news.content,
       image: getImageUrl(news.image),
       created_at: news.created_at,
+      reporter: {
+        id: news?.user.id,
+        name: news?.user.name,
+        profile:
+          news?.user?.profile != null
+            ? getImageUrl(news?.user?.profile)
+            : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTALYMcTYF3DExgHovymgM3aXLNOAj-xmQMAl7eCwne5Q&s",
+      },
     };
   }
 }
