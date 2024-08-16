57b34266c6581af46c2b8aacbe004717bbdd0948
Mohit Soni <developer_mohitsoni@outlook.com>
2024-08-16 11:57:34 +0530

    Add store functionality of news and validate the newsSchema

--- controllers/NewsController.js
+++ controllers/NewsController.js
@@ -1,7 +1,39 @@
+import vine from "@vinejs/vine";
+import { newsSchema } from "../validations/newsValidation";
+
 class NewsController {
   static async index(req, res) {}
 
-  static async store(req, res) {}
+  static async store(req, res) {
+    try {
+      const user = req.user;
+
+      const body = req.body;
+
+      const validator = vine.compile(newsSchema);
+
+      const payload = await validator.validate(body);
+
+      return res.json({
+        payload,
+      });
+    } catch (error) {
+      console.log("The error is: ", error);
+
+      if (error instanceof errors.E_VALIDATION_ERROR) {
+        // console.log(error.messages);
+
+        return res.status(400).json({
+          errors: error.messages,
+        });
+      } else {
+        return res.status(500).json({
+          status: 500,
+          message: "Something went wrong... Please try again",
+        });
+      }
+    }
+  }
 
   static async show(req, res) {}
 
