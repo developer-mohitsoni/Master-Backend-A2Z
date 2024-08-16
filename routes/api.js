e268e3a590ffb60b611c0829625679a804ce0f07
Mohit Soni <developer_mohitsoni@outlook.com>
2024-08-16 12:11:43 +0530

    Add routes for newsController

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,24 @@ import AuthController from "../controllers/AuthController.js";
 import ProfileController from "../controllers/ProfileController.js";
 
 import authMiddleware from "../middleware/Authenticate.js";
+import NewsController from "../controllers/NewsController.js";
 
 const router = Router();
 
 router.post("/auth/register", AuthController.register);
-router.post("/auth/login", AuthController.login)
-
+router.post("/auth/login", AuthController.login);
 
 //*  Profile routes
 
-router.get("/profile", authMiddleware, ProfileController.index) //! Private Route
-router.put("/profile/:id", authMiddleware, ProfileController.update) //! Private Route
+router.get("/profile", authMiddleware, ProfileController.index); //! Private Route
+router.put("/profile/:id", authMiddleware, ProfileController.update); //! Private Route
+
+//* News routes
+
+router.get("/news", NewsController.index);
+router.post("/news", authMiddleware, NewsController.store);
+router.get("/news/:id", NewsController.show);
+router.put("/news/:id", NewsController.update);
+router.delete("/news/:id", NewsController.destroy);
 
 export default router;
