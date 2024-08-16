5f99fa484dc445d50d198db343669b86457be253
Mohit Soni <developer_mohitsoni@outlook.com>
2024-08-16 14:07:55 +0530

    Complete newsApiTransform function

--- transform/newsApiTransform.js
+++ transform/newsApiTransform.js
@@ -1,10 +1,14 @@
-class newsApiTransform{
-    static transform(news){
-        return {
-            id: news.id,
-            heading: news.title,
-            news: news.content,
-            image: news.image
-        }
-    }
+import { getImageUrl } from "../utils/helper.js";
+
+class newsApiTransform {
+  static transform(news) {
+    return {
+      id: news.id,
+      heading: news.title,
+      news: news.content,
+      image: getImageUrl(news.image),
+    };
+  }
 }
+
+export default newsApiTransform;
