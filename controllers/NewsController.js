8794e81636c4207ea7a395cd9a3565c6b0dfb604
Mohit Soni <developer_mohitsoni@outlook.com>
2024-08-16 12:57:08 +0530

    Complete setup for store the image and validate the news functionality

--- controllers/NewsController.js
+++ controllers/NewsController.js
@@ -1,6 +1,10 @@
 import vine, { errors } from "@vinejs/vine";
 import { newsSchema } from "../validations/newsValidation.js";
 
+import prisma from "../DB/db.config.js";
+
+import { generateRandomNum, imageValidator } from "../utils/helper.js";
+
 class NewsController {
   static async index(req, res) {}
 
@@ -14,8 +18,52 @@ class NewsController {
 
       const payload = await validator.validate(body);
 
+      if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({
+          errors: {
+            image: "Image field is required",
+          },
+        });
+      }
+
+      const image = req.files?.image;
+
+      //* Image custom validator
+      const message = imageValidator(image?.size, image?.mimetype);
+
+      if (message !== null) {
+        return res.status(400).json({
+          errors: {
+            image: message,
+          },
+        });
+      }
+
+      //* Image Upload
+      // imgExt => returning an array by spliting the name of profile image i.e firstOne => Name of file secondOne => extension of profile image
+      const imgExt = image?.name.split(".");
+
+      // This will contains the randomnumber with profile image extension as:- 545542.img, 5453154.wbpeg, 54240.jpeg etc.
+      const imageName = generateRandomNum() + "." + imgExt[1];
+
+      // Now this will upload our image in the given directory
+      const uploadPath = process.cwd() + "/public/images/" + imageName;
+
+      image.mv(uploadPath, (err) => {
+        if (err) throw err;
+      });
+
+      payload.image = imageName;
+      payload.user_id = user.id;
+
+      const news = await prisma.news.create({
+        data: payload,
+      });
+
       return res.json({
-        payload,
+        status: 200,
+        message: "News Created Successfully!",
+        news,
       });
     } catch (error) {
       console.log("The error is: ", error);
