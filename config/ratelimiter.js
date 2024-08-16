2bed0ddcfac4f5c99f398ef095894793fd36ba6f
Mohit Soni <developer_mohitsoni@outlook.com>
2024-08-16 17:59:13 +0530

    Update rate limiter configuration for longer window and higher limit
    The code changes in `ratelimiter.js` update the rate limiter configuration to use a longer window of 1 hour (3600000 milliseconds) and a higher limit of 100 requests per IP address. This change improves the rate limiting functionality by allowing more requests within a longer time frame, enhancing the performance of the application. Additionally, it ensures better protection against abuse and excessive requests.

--- config/ratelimiter.js
+++ config/ratelimiter.js
@@ -1,8 +1,8 @@
 import rateLimit from "express-rate-limit";
 
 export const limitter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 1 minutes
-	limit: 1, // Limit each IP to 1 requests per `window` (here, per 1 minutes).
+	windowMs: 60 * 60 * 1000, // 3600000/60000 => 60 min => 1hr
+	limit: 100, // Limit each IP to 1 requests per `window` (here, per 1 h).
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	// store: ... , // Redis, Memcached, etc. See below.
