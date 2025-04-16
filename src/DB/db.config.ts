2dd0484db943226c6e15421f5b804aa0a6abd27a
Mohit Soni <developer_mohitsoni@outlook.com>
2025-04-16 21:36:58 +0530

    Refactor db.config.ts to import PrismaClient from the correct path and extend with optimize functionality

--- src/DB/db.config.ts
+++ src/DB/db.config.ts
@@ -1,8 +1,11 @@
-import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { withOptimize } from "@prisma/extension-optimize";
+import { PrismaClient } from "../../generated/prisma/index.js";
 
 const prisma = new PrismaClient({
 	log: ["query", "error"]
-}).$extends(withAccelerate());
+})
+	.$extends(withAccelerate())
+	.$extends(withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY as string }));
 
 export default prisma;
