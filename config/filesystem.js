c6b054c613106f2f8d68767fcea0375cf3bd5eb6
Mohit Soni <developer_mohitsoni@outlook.com>
2025-04-16 21:10:30 +0530

    Remove obsolete configuration and controller files

--- config/filesystem.js
+++
@@ -1,9 +0,0 @@
-// Ye supported image mime types ka array hai
-export const supportedMimes = [
-  "image/png",
-  "image/jpg",
-  "image/jpeg",
-  "image/svg",
-  "image/gif",
-  "image/webp",
-];
--- jobs/index.js
+++
@@ -1,1 +0,0 @@
-import "./SendEmailJob.js"
