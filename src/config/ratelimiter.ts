9c1c5882a84a9346a133e4e200863d35c60e72dd
Mohit Soni <developer_mohitsoni@outlook.com>
2025-04-18 20:23:59 +0530

    Update rate limiter configuration to restrict requests to 5 per IP every 5 minutes

--- src/config/ratelimiter.ts
+++ src/config/ratelimiter.ts
@@ -1,10 +1,13 @@
 import rateLimit from "express-rate-limit";
 
 export const limitter = rateLimit({
-	windowMs: 60 * 60 * 1000,
-
-	limit: 100,
-
+	windowMs: 5 * 60 * 1000, // 5 minutes
+	limit: 5, // Max 5 requests per IP in 5 mins
 	standardHeaders: "draft-7",
-	legacyHeaders: false
+	legacyHeaders: false,
+	message: {
+		status: 429,
+		message: "Too many login attempts, please try again later."
+	}
+	// store: new RedisStore({ sendCommand: redisClient.sendCommand.bind(redisClient) }), // If using Redis
 });
