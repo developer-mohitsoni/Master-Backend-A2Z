e0b8df30818fc693198eff906b82a94272bf45d6
Mohit Soni <developer_mohitsoni@outlook.com>
2025-04-18 11:05:15 +0530

    Refactor api routes to uncomment email verification route and comment out unused routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,12 +1,16 @@
 import { Router } from "express"; // Import Router
-import cacheMiddleware from "../config/redisCache.js";
+// import cacheMiddleware from "../config/redisCache.js";
 import AuthController from "../controllers/AuthController.js";
 import MailController from "../controllers/MailController.js";
-import NewsController from "../controllers/NewsController.js";
-import ProfileController from "../controllers/ProfileController.js";
-import authMiddleware from "../middleware/Authenticate.js";
+// import NewsController from "../controllers/NewsController.js";
+// import ProfileController from "../controllers/ProfileController.js";
+// import authMiddleware from "../middleware/Authenticate.js";
+
+import VerificationEmailController from "../controllers/VerificationEmailController.js"; // Import VerificationEmailController
 
 const router = Router();
+
+router.get("/auth/verify-email", VerificationEmailController.verifyMailHandler); // Email verification route
 
 router.post("/auth/register", AuthController.registerHandler);
 
@@ -16,15 +20,15 @@ router.get("/send-email", MailController.mailHandler);
 
 //*  Profile routes
 
-router.get("/profile", authMiddleware, ProfileController.indexHandler); //! Private Route
-router.put("/profile/:id", authMiddleware, ProfileController.update); //! Private Route
+// router.get("/profile", authMiddleware, ProfileController.indexHandler); //! Private Route
+// router.put("/profile/:id", authMiddleware, ProfileController.update); //! Private Route
 
 //* News routes
 
-router.get("/news", cacheMiddleware, NewsController.indexHandler);
-router.post("/news", authMiddleware, NewsController.storeHandler);
-router.get("/news/:id", cacheMiddleware, NewsController.showHandler);
-router.put("/news/:id", authMiddleware, NewsController.updateHandler);
-router.delete("/news/:id", authMiddleware, NewsController.destroyHandler);
+// router.get("/news", cacheMiddleware, NewsController.indexHandler);
+// router.post("/news", authMiddleware, NewsController.storeHandler);
+// router.get("/news/:id", cacheMiddleware, NewsController.showHandler);
+// router.put("/news/:id", authMiddleware, NewsController.updateHandler);
+// router.delete("/news/:id", authMiddleware, NewsController.destroyHandler);
 
 export default router;
