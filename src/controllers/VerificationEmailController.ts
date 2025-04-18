9e26f7ea93c5fa1206232e5b04909568a94a7413
Mohit Soni <developer_mohitsoni@outlook.com>
2025-04-18 12:12:59 +0530

    Refactor email verification logic to utilize JWT for token validation and user retrieval

--- src/controllers/VerificationEmailController.ts
+++ src/controllers/VerificationEmailController.ts
@@ -1,4 +1,5 @@
 import prisma from "@/DB/db.config.js";
+import type { MyJwtPayload } from "@/types/index.js";
 import type { NextFunction, Request, RequestHandler, Response } from "express";
 import jwt from "jsonwebtoken";
 
@@ -8,39 +9,64 @@ class VerificationEmailController {
 
 		if (!token) return res.status(400).json({ message: "Token missing" });
 
-		const user = await prisma.users.findFirst({
-			where: { verificationToken: String(token) }
-		});
+		try {
+			jwt.verify(
+				token as string,
+				process.env.JWT_SECRET as string,
+				(err: jwt.VerifyErrors | null, user: any) => {
+					if (err)
+						return res
+							.status(401)
+							.json({ status: 401, message: "Unauthorized" });
+					req.user = user as MyJwtPayload;
+				}
+			);
 
-		if (!user) {
-			return res.status(400).json({ message: "Invalid or Expired token" });
+			const decoded = req.user;
+			const user = await prisma.users.findUnique({
+				where: { id: decoded?.userId }
+			});
+
+			if (!user) return res.status(400).json({ message: "Invalid user" });
+
+			if (user.emailVerified) {
+				// Email already verified, return fresh login token
+				const accessToken = jwt.sign(
+					{ userId: user.id },
+					process.env.JWT_SECRET as string,
+					{
+						expiresIn: "1d"
+					}
+				);
+
+				return res
+					.status(200)
+					.json({ message: "Already verified", token: accessToken });
+			}
+
+			await prisma.users.update({
+				where: { id: user.id },
+				data: {
+					emailVerified: true,
+					verificationToken: token as string,
+				}
+			});
+
+			// ✅ Auto login token return
+			const accessToken = jwt.sign(
+				{ userId: user.id },
+				process.env.JWT_SECRET as string,
+				{
+					expiresIn: "1d"
+				}
+			);
+
+			return res
+				.status(200)
+				.json({ message: "Email verified", token: accessToken });
+		} catch (err) {
+			return res.status(400).json({ message: "Invalid or expired token" });
 		}
-
-		await prisma.users.update({
-			where: { id: user.id },
-			data: {
-				emailVerified: true,
-				verificationToken: null
-			}
-		});
-
-		// Generate JWT for auto-login UX
-		const jwtToken = jwt.sign(
-			{
-				userId: user.id
-			},
-			process.env.JWT_SECRET as string,
-			{
-				expiresIn: "7d"
-			}
-		);
-
-		// Send response with JWT token
-		return res.status(200).json({
-			success: true,
-			message: "Email verified successfully.",
-			token: jwtToken
-		});
 	}
 
 	static verifyMailHandler: RequestHandler = async (
